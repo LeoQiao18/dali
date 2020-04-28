69a2095941cd79a4277e545c42c1db9128e0b333
Leo Qiao <qiaofeitong@hotmail.com>
2020-04-28 12:40:27 -0400

    add glfw

--- dali/src/app.cpp
+++ dali/src/app.cpp
@@ -5,6 +5,8 @@
 #include <dali/app.h>
 #include <dali/event.h>
 #include <dali/log.h>
+
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 namespace dali {
@@ -18,6 +20,9 @@ namespace dali {
     }
 
     void App::run() {
+        int major, minor, rev;
+        glfwGetVersion(&major, &minor, &rev);
+        DALI_CORE_INFO("GLFW version: {}.{}.{}", major, minor, rev);
         WindowResizeEvent e(100, 200);
         if (e.get_type() == WindowResizeEvent::get_static_type()) {
             DALI_CORE_TRACE(e);
